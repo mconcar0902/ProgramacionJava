1276f0cecd214db714866b992c730b4a187862bd
marcos contreras <5marcoscontreras5@gmail.com>
2025-09-22 13:08:34 +0200

    first commit

---
+++ src/Ejercicios_Bloque_01/Ejercicio02_PidoTresNumerosDevuelveDeUnoEnUno.java
@@ -0,0 +1,27 @@
+package Ejercicios_Bloque_01;
+
+import java.util.Scanner;
+
+public class Ejercicio02_PidoTresNumerosDevuelveDeUnoEnUno {
+
+	
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Escribe un numero entero: ");
+		int NumEntero = sc.nextInt();
+		System.out.println("Su numero entero es " + NumEntero);
+		
+		System.out.print("Escribe un numero flotante: ");
+		float NumFloat = sc.nextFloat();
+		System.out.println("Su numero flotante es " + NumFloat);
+		
+		System.out.print("Escribe un numero doble: ");
+		double NumDoble = sc.nextDouble();
+		System.out.println("Su numero doble es " + NumDoble);
+		
+	}
+
+}
