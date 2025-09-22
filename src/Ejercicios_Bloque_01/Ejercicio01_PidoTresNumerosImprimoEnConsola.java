18e36a669b6c08f91b86620473dde27773e08a5a
marcos contreras <5marcoscontreras5@gmail.com>
2025-09-22 12:59:49 +0200

    first commit

---
+++ src/Ejercicios_Bloque_01/Ejercicio01_PidoTresNumerosImprimoEnConsola.java
@@ -0,0 +1,24 @@
+package Ejercicios_Bloque_01;
+
+import java.util.Scanner;
+
+public class Ejercicio01_PidoTresNumerosImprimoEnConsola {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Escribe un numero de tipo entero: ");
+		int NumEntero = sc.nextInt();
+		
+		System.out.print("Escribe un numero de tipo flotante: ");
+		float NumFlotante = sc.nextFloat(); 
+		
+		System.out.print("Escribe un numero de tipo doble: ");
+		double NumDoble = sc.nextDouble();
+		
+		System.out.println("El numero entero es " + NumEntero + ", el numero flotante es "+ NumFlotante + " y el numero doble es "+ NumDoble+".");
+		
+	}
+
+}
