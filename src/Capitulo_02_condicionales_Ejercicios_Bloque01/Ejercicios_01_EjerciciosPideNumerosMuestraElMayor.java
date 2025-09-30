0eb6d9689b7b7772fd8bc4435e964fe5e5b9ed35
marcos contreras <5marcoscontreras5@gmail.com>
2025-09-30 14:17:42 +0200

    first commit

---
+++ src/Capitulo_02_condicionales_Ejercicios_Bloque01/Ejercicios_01_EjerciciosPideNumerosMuestraElMayor.java
@@ -0,0 +1,25 @@
+package Capitulo_02_condicionales_Ejercicios_Bloque01;
+
+import java.util.Scanner;
+
+public class Ejercicios_01_EjerciciosPideNumerosMuestraElMayor {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Escibe el primer numero: ");
+		int num = sc.nextInt();
+		
+		System.out.println("Escibe el segundo numero: ");
+		int num1 = sc.nextInt();
+		
+		if (num > num1) {
+			System.out.println("El numero mayor es " + num);
+		}
+		else if (num < num1){
+			System.out.println("El numero mayor es " + num1);
+		}
+	}
+
+}
